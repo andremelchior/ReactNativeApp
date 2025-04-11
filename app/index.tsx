a7daf3ba321e611ebc28da443461ef4c4690e97d
André Melchior <andre.silva468@fatec.sp.gov.br>
2025-04-10 22:23:51 -0300

    feat: add text input for set name, import components 'CustomButton' and 'Form'

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import CustomButton from "@/components/CustomButton";
+import Form from "@/components/Form";
 import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
-import CustomButton from "./Title";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function HomeScreen(){
     const VALUE = 0;
+    const NOME = 'André';
     const [count, setCount] = useState(VALUE);
+    const [nome, setName] = useState(NOME);
 
     function alterarValor(valor: number){
         return count + valor 
@@ -12,7 +15,7 @@ export default function HomeScreen(){
 
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10}}>
-            <Text>Hello World!</Text>
+            <Text>Bem-vindo ao app, {nome}!</Text>
 
             <Text>Contador: {count}</Text>
             {/* <Button title="Aumentar" onPress={() => {setCount(count + 1)}}/>
@@ -22,7 +25,19 @@ export default function HomeScreen(){
             <Button title="Aumentar" onPress={() => {setCount(alterarValor(1))}}/>
             <Button title="Diminuir" onPress={() => {setCount(alterarValor(-1))}}/>
             <Button title="Resetar" onPress={() => {setCount(VALUE)}}/>
-            <CustomButton title="Nome"/> {/*teste de botão*/}
+
+            <TextInput
+                placeholder="Digite seu nome: "
+                onChangeText={setName}
+            />
+
+            <CustomButton 
+                title='botão vermelho'
+                onPress={() => {setName('André ' + count)}}
+            />
+
+           
+            <Form title='Digite algo'/>
         </View>
     );
 }
