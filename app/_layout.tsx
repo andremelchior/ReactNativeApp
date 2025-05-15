b783303d3c5dd6b3b787e419a70d8af8cb61638e
André Melchior <andre.melchior@outlook.com>
2025-05-15 20:38:19 -0300

    feat: add tabs screens from expo router for home, about and settings screen navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,35 @@
 import React from "react";
-import { Slot } from "expo-router";
+import { Slot, Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Layout(){
-  return <Slot/>;
+  return (
+    <Tabs screenOptions={{tabBarActiveTintColor: 'Blue'}}>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Inicio',
+          tabBarIcon:
+          ({color, size}) => <Ionicons name='home-outline' size={size} color={color}/>
+        }}
+      />
+      <Tabs.Screen
+        name='about'
+        options={{
+          title: 'Sobre',
+          tabBarIcon:
+          ({color, size}) => <Ionicons name='information-outline' size={size} color={color}/>
+        }}
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          title: 'Configurações',
+          tabBarIcon:
+          ({color, size}) => <Ionicons name='settings-outline' size={size} color={color}/>
+        }}
+      />
+        
+    </Tabs>
+  );
 }
